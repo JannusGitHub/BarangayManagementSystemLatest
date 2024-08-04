a3f9afc935de120351db8e6117040be59a6f5ccc
JannusGitHub <jannushernandez@gmail.com>
2024-08-05 00:32:31 +0800

    First Commit

---
+++ resources/views/barangay_clearance_certificate_report_pdf.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Barangay License and Permit Certificate</title>
+</head>
+<style>
+    table, th, td {
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+</style>
+<body>
+    <h1 style="text-align: center; font-size: 20px;" ><strong>{{ $repub_title }}</strong></h1>
+    <p style="text-align: center; font-size: 15px;">{{ $province_title }}<br>
+        <span>{{ $city_title }}</span><br>
+        <span style="font-size: 18px; color:dodgerblue;"><strong>{{ $brgy_title }}</strong></span><br>
+        <span style="font-size: 12px;"><strong>{{ $telephone_title }}</strong></span>
+    </p>
+
+    <hr style="width:100%;text-align:left;margin-left:0">
+    <h2 style="text-align: center;"><strong>LIST OF BARANGAY CLEARANCE CERTIFICATES</strong><br>
+    <table style="width:100%">
+        <tr>
+            <th style="text-transform: uppercase; white-space: normal; width: 100px; border: 1px solid black;">OR Number</th>
+            <th style="text-transform: uppercase; white-space: normal; width: 120px; border: 1px solid black;">Requested By</th>
+            <th style="text-transform: uppercase; white-space: normal; width: 100px; border: 1px solid black;">Purpose</th>
+            <th style="text-transform: uppercase; white-space: normal; width: 120px; border: 1px solid black;">Amount Collection</th>
+            <th style="text-transform: uppercase; white-space: normal; width: 120px; border: 1px solid black;">Issued On</th>
+            <th style="text-transform: uppercase; white-space: normal; width: 100px; border: 1px solid black;">Remarks</th>
+        </tr>
+
+        @foreach ($data as $row)
+            <tr>
+                <td style="white-space: normal; border: 1px solid black;">{{ $row->or_number }}</td>
+                <td style="white-space: normal; border: 1px solid black; text-transform:capitalize;">{{ $row->resident_info->user_info->firstname ." ". $row->resident_info->user_info->lastname }}</td>
+                <td style="white-space: normal; border: 1px solid black;">{{ $row->purpose }}</td>
+                @php
+                    $date=date_create($row->issued_on);
+                    $issued_on = date_format($date,"Y-m-d");
+                @endphp
+                <td style="white-space: normal; border: 1px solid black;">{{ $row->amount_collection }}</td>
+                <td style="white-space: normal; border: 1px solid black;">{{ $issued_on }}</td>
+                <td style="white-space: normal; border: 1px solid black;">{{ $row->remarks }}</td>
+            </tr>
+        @endforeach
+
+    </table>
+
+
+
+
+</body>
+</html>
